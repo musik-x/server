b57ac3a263a4701a1d0add70b1cffd9fc6bb450e
Mahdihrs <mahdiharis@outlook.com>
2019-03-07 17:59:17 +0700

    google sign in

---
+++ middlewares/authentication.js
@@ -0,0 +1,36 @@
+const { decode } = require('../helpers/jwt')
+
+function authentication(req, res, next) {
+    // console.log(req.headers, 'headerss')
+    if (!req.headers.access_token) {
+        res
+            .status(401)
+            .json({
+                msg: `Unauthorized Access`
+            })
+    } else {
+        try {
+            let decoding = decode(req.headers.access_token)
+            req.decoded = decoding
+            next()
+        } catch(err) {
+            if (JSON.stringify(err.name) == 'JsonWebTokenError') {
+                res
+                    .status(401)
+                    .json({
+                        msg: `Unauthorized Access: Invalid Token`,
+                        err: err
+                    })
+            } else {
+                res
+                    .status(401)
+                    .json({
+                        msg: `Unauthorized Access: Invalid Token`,
+                        err: err
+                    })
+            }
+        }
+    }
+}
+
+module.exports = authentication
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/users.js')
+const authentication = require('../middlewares/authentication')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/login', userController.login)
 
 module.exports = router;
